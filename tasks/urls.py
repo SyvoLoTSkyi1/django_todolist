bb92d75ec43977aa5c4dfaafdef25e0056276a49
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-09-18 11:53:43 +0300

    category views and templates, changes in task views and templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import urls
 from django.urls import path, include
 
-from tasks.views import TaskView, TaskDetailView, CreateTaskView, UpdateTaskView, DeleteTaskView
+from tasks.views import TaskView, TaskDetailView, CreateTaskView, \
+    UpdateTaskView, DeleteTaskView, CreateCategoryView, \
+    UpdateCategoryView, DeleteCategoryView
 
 urlpatterns = [
     path('task_list/', TaskView.as_view(), name='task_list'),
@@ -9,5 +11,7 @@ urlpatterns = [
     path('task-create/', CreateTaskView.as_view(), name='task-create'),
     path('task-update/<int:pk>/', UpdateTaskView.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteTaskView.as_view(), name='task-delete'),
+    path('category-create/', CreateCategoryView.as_view(), name='category-create'),
+    path('category-update/<int:pk>/', UpdateCategoryView.as_view(), name='category-update'),
+    path('category-delete/<int:pk>/', DeleteCategoryView.as_view(), name='category-delete'),
 ]
-
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from tasks.models import Task
+from tasks.models import Task, Category
+from tasks.model_forms import TaskForm
 
 
 class TaskView(LoginRequiredMixin, ListView):
@@ -13,6 +13,7 @@ class TaskView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['categories'] = Category.objects.filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
@@ -24,7 +25,42 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
 
 class CreateTaskView(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'description', 'category', 'complete']
+    form_class = TaskForm
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class UpdateTaskView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class DeleteTaskView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy('task_list')
+
+
+class CreateCategoryView(LoginRequiredMixin, CreateView):
+    model = Category
+    fields = ['title', 'description']
     success_url = reverse_lazy('task_list')
 
     def form_valid(self, form):
@@ -32,12 +68,12 @@ class CreateTaskView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class UpdateTaskView(LoginRequiredMixin, UpdateView):
-    model = Task
-    fields = ['title', 'description', 'category', 'complete']
+class UpdateCategoryView(LoginRequiredMixin, UpdateView):
+    model = Category
+    fields = ['title', 'description']
     success_url = reverse_lazy('task_list')
 
 
-class DeleteTaskView(LoginRequiredMixin, DeleteView):
-    model = Task
+class DeleteCategoryView(LoginRequiredMixin, DeleteView):
+    model = Category
     success_url = reverse_lazy('task_list')
