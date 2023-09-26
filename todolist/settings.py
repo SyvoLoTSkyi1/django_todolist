c9cc7c815f3d9c008c33facc7c45f1d542b7817c
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-09-26 12:16:14 +0300

    add css

--- todolist/settings.py
+++ todolist/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+STATICFILES_DIRS = ['static_dev']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
